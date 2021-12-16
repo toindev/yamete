a9a48a3941dc8b813ee9266a02b35f3902055e2f
Romain Prévost <32127074+toindev@users.noreply.github.com>
2021-12-16 16:16:54 +0100

    DL sounds directly from GH

--- set_yamete_usb.bat
+++ set_yamete_usb.bat
@@ -1,9 +1,12 @@
 
 mkdir %USERPROFILE%\yamete
-copy .\ah.wav %USERPROFILE%\yamete\ah.wav
-copy .\yamete.wav %USERPROFILE%\yamete\yamete.wav
+cd %USERPROFILE%\yamete
+powershell Invoke-WebRequest https://github.com/toindev/yamete/raw/main/ah.wav -OutFile ah.wav
+powershell Invoke-WebRequest https://github.com/toindev/yamete/raw/main/yamete.wav -OutFile yamete.wav
 
-@reg add "HKEY_CURRENT_USER\AppEvents\Schemes"                                             /t REG_SZ /d ".KanyeTest"  /f
+@reg add "HKEY_CURRENT_USER\AppEvents\Schemes"                                             /t REG_SZ /d "Yamete"  /f
 @reg add "HKEY_CURRENT_USER\AppEvents\Schemes\Apps\.Default\.Default\.Current"             /t REG_SZ /d ""       /f
-@reg add "HKEY_CURRENT_USER\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current"        /t REG_SZ /d %USERPROFILE%\yamete\yamete.wav       /f
-@reg add "HKEY_CURRENT_USER\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current"      /t REG_SZ /d %USERPROFILE%\yamete\ah.wav       /f
+@reg add "HKEY_CURRENT_USER\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current"        /t REG_SZ /d "%USERPROFILE%\yamete\yamete.wav"       /f
+@reg add "HKEY_CURRENT_USER\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current"      /t REG_SZ /d "%USERPROFILE%\yamete\ah.wav"       /f
+
+Write-Output Done!
